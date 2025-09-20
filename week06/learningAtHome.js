5b0fc6bf2b4413b9188404b89a8558525e92b44e
Parinda Dear <142294668+ParindaDear@users.noreply.github.com>
2025-09-20 20:05:33 +0700

    More Practice at home (20 Sep 2025)

--- week06/learningAtHome.js
+++ week06/learningAtHome.js
@@ -31,3 +31,62 @@ function outer(){
 }
 outer()
 console.log(x)
+
+/*Write a function createCounter(start) that:
+Starts counting from the given start value.
+Returns an object with three methods:
+increment() → increases the counter by 1
+decrement() → decreases the counter by 1
+getValue() → returns the current counter value
+You must use closure to store the counter value privately (not accessible directly). 
+Expected Output:
+const counter = createCounter(5)
+console.log(counter.increment()) // 6
+console.log(counter.increment()) // 7
+console.log(counter.decrement()) // 6
+console.log(counter.getValue())  // 6  */
+function createCounter(start) {
+  let count = start   // ตัวแปรนี้ถูกเก็บใน closure
+
+  return {
+    increment: function() {
+      count++
+      return count
+    },
+    decrement: function() {
+      count--
+      return count
+    },
+    getValue: function() {
+      return count
+    }
+  }
+}
+
+const counter = createCounter(5)
+console.log(counter.increment()) 
+console.log(counter.increment()) 
+console.log(counter.decrement()) 
+console.log(counter.getValue()) 
+
+/*Write a function secretMessage(password) that:
+1. Accepts a string password.
+2. Returns a function that accepts another string tryPassword.
+3. If tryPassword matches the original password, return "Access granted".
+4. Otherwise, return "Access denied".
+Example Output 
+const check = secretMessage("opensesame")
+console.log(check("wrongpass"))   // Access denied
+console.log(check("opensesame"))  // Access granted */
+function secretMessage(password) {
+  return function(tryPassword) {
+    if (tryPassword === password) {
+      return "Access granted"
+    } else {
+      return "Access denied"
+    }
+  }
+}
+const check = secretMessage("opensesame")
+console.log(check("wrongpass"))  
+console.log(check("opensesame")) 
