8ae93e8601abec552a279465b2ebaafb8f3eff2a
Parinda Dear <142294668+ParindaDear@users.noreply.github.com>
2025-10-16 09:03:27 +0700

    Learning from Ajarn Answer key

--- week09/Practice-Chapter6/01/main.js
+++ week09/Practice-Chapter6/01/main.js
@@ -1,33 +1,59 @@
-const form = document.querySelector("form")
-const username = document.getElementById("username")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const confirmPassword = document.getElementById("confirm-password")
-const message = document.querySelector("p")
+const createBtn = document.getElementById('submit')
+createBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  const allInputEles = document.querySelectorAll('input')
+  const isValidInput = Array.from(allInputEles).every(
+    (inputEle) => inputEle.value.trim().length !== 0
+  )
+  const pEle = document.querySelector('p')
+  if (isValidInput) {
+    pEle.textContent = 'your data completed'
+    pEle.style.color = 'green'
+  } else {
+    pEle.textContent = 'missing some values, please try again!'
+    pEle.style = 'color:red'
+  }
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
+  const password = document.getElementById('password').value.trim()
+  const confirmPassword = document
+    .getElementById('confirm-password')
+    .value.trim()
+  if (password !== confirmPassword) {
+    pEle.setAttribute('style', 'color:red')
+    pEle.textContent = 'password and confirm do not match, check again'
+  }
+})
+// ทำเอง
+// const form = document.querySelector("form")
+// const username = document.getElementById("username")
+// const email = document.getElementById("email")
+// const password = document.getElementById("password")
+// const confirmPassword = document.getElementById("confirm-password")
+// const message = document.querySelector("p")
 
-    const user = username.value.trim()
-    const mail = email.value.trim()
-    const pass = password.value.trim()
-    const confirm = confirmPassword.value.trim()
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault()
 
-    //ตรวจ field ว่าง
-    if (!user || !mail || !pass || !confirm) {
-      message.style.color = "red"
-      message.textContent = "missing some values, please try again!"
-      return
-    }
+//     const user = username.value.trim()
+//     const mail = email.value.trim()
+//     const pass = password.value.trim()
+//     const confirm = confirmPassword.value.trim()
 
-    // รหัสผ่านตรงกันมั้ย
-    if (pass !== confirm) {
-      message.style.color = "red"
-      message.textContent = "password and confirm do not match, check again"
-      return;
-    }
+//     //ตรวจ field ว่าง
+//     if (!user || !mail || !pass || !confirm) {
+//       message.style.color = "red"
+//       message.textContent = "missing some values, please try again!"
+//       return
+//     }
 
-    //ผ่านทุกเงื่อนไข
-    message.style.color = "green";
-    message.textContent = "your data completed";
-  });
+//     // รหัสผ่านตรงกันมั้ย
+//     if (pass !== confirm) {
+//       message.style.color = "red"
+//       message.textContent = "password and confirm do not match, check again"
+//       return;
+//     }
+
+//     //ผ่านทุกเงื่อนไข
+//     message.style.color = "green";
+//     message.textContent = "your data completed";
+//   });
--- week09/Practice-Chapter6/02/main.js
+++ week09/Practice-Chapter6/02/main.js
@@ -1,13 +1,30 @@
-const inputElement = document.getElementById('keyInput')
 const keyLog = document.getElementById('keyLog')
 
-inputElement.addEventListener('keydown', (event) =>{
-    const p = document.createElement('p')
-    if(event.key === 'Enter'){
-        p.textContent  = "You pressed: Enter "
-        p.style.color = "blue"
-    } else {
-        p.textContent = `You pressed: ${event.key}`
-    }
-    keyLog.appendChild(p)
+document.addEventListener('keydown', (event) => {
+  let keyPressed = event.key
+  let message = `You pressed: ${keyPressed}`
+  const pEle = document.createElement('p')
+
+  if (keyPressed === 'Enter') {
+    message = 'You pressed: Enter'
+    pEle.style.color = 'blue'
+  } else keyLog.style.color = 'black'
+  pEle.textContent = message
+  keyLog.appendChild(pEle)
 })
+
+
+// ทำเอง
+// const inputElement = document.getElementById('keyInput')
+// const keyLog = document.getElementById('keyLog')
+
+// inputElement.addEventListener('keydown', (event) =>{
+//     const p = document.createElement('p')
+//     if(event.key === 'Enter'){
+//         p.textContent  = "You pressed: Enter "
+//         p.style.color = "blue"
+//     } else {
+//         p.textContent = `You pressed: ${event.key}`
+//     }
+//     keyLog.appendChild(p)
+// })
